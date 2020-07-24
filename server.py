2b77a235e8112e019f4179170188c8687523f698
Saloni Bobade <44300604+salonibobade@users.noreply.github.com>
2020-07-24 23:59:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,31 @@
 from flask import Flask,render_template,request,redirect,url_for
 import MySQLdb
+from flask_cors import CORS,cross_origin
+import flask
 
 
 app = Flask(__name__)
+CORS(app)
 conn = MySQLdb.connect(host="localhost",user="root",password="Sallu@1811",db="testdb")
 
 @app.route("/loggedin",methods=["POST",'GET'])
 def login():
-    user = request.args.get('user')
-    #username = str(request.form["user"])
-   # password = str(request.form["password"])
-   #cursor = conn.cursor()
-   # cursor.execute("SELECT * FROM user WHERE username ='"+username+"'and password='"+password+"'")
-   # user = cursor.fetchone()
-   # if user is None:
-    #    return False 
+
+    username = request.args.get('user')
+    password=request.args.get('password')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user WHERE username ='"+username+"'and password='"+password+"'")
+    user = cursor.fetchone()
+
+    if user is None:
+        response = flask.jsonify({"status":"false"})
+        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:4200')
+        return response
+    response = flask.jsonify({"status":"true"})
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:4200')
     
-    return user
+    return response
+    #return user
 
 
 
@@ -24,39 +33,3 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-
-'''from flask import Flask, request
-from flask_cors import CORS, cross_origin
-from flask_restful import Resource, Api
-from flask import Flask,render_template,request,redirect,url_for
-import MySQLdb
-
-
-app = Flask(__name__)
-api=Api(app)
-conn = MySQLdb.connect(host="localhost",user="root",password="Sallu@1811",db="testdb")
-
-
-
-@app.route("/loggedin",methods=["POST",'GET'])
-def login():
-    username = str(request.form["user"])
-    password = str(request.form["password"])
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM user WHERE Emp_ID ='"+username+"'and Emp_Password='"+password+"'")
-    user = cursor.fetchone()
-    if user is None:
-        return False
-
-    return True
-    '''
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-   app.run(port=5002)
